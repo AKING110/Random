6d4a232cedbb84044589b4a281c3e986fd6ccedb
IMTIAZ AKING <98144321+AKING110@users.noreply.github.com>
2022-07-30 19:09:32 +0500

    Update AKING.py

--- AKING.py
+++ AKING.py
@@ -1,15 +1,15 @@
 import os, platform, time
-print('\n\x1b[1;37m Checking Update...');time.sleep(0.5)
+print('\n\x1b[1;37m[•] Checking Update...');time.sleep(0.5)
 os.system('git pull')
 def Run():
         bit = platform.architecture()[0]
         if bit == '64bit':
-            print("\x1b[1;92m Congratulations ! Your Device Support this Tools")
+            print("\x1b[1;92m[•] Congratulations ! Your Device Support this Tools")
             print('[•] Join Over Facebook Group First')
             os.system('xdg-open https://facebook.com/groups/351076900316263/')
             import PAK
         elif bit == '32bit':
-            print("\n\x1b[1;92m Congratulations ! Your Device Support this Tools")
+            print("\n\x1b[1;92m[•] Congratulations ! Your Device Support this Tools")
             print('[•] Join Over Facebook Group First')
             os.system('xdg-open https://facebook.com/groups/351076900316263/')
             import PAK32
